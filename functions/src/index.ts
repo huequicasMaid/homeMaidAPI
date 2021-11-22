a8fe0b737c7628ae4c23f74b1a8fa7ff387c2d81
huequica <dev@huequica.xyz>
2021-11-23 03:33:30 +0900

    build sample functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+
+type response = {
+  status: number;
+  message: string;
+};
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((_request, response) => {
+  functions.logger.info('Hello logs!', {structuredData: true});
+  const res: response = {
+    status: 200,
+    message: 'Hello from Firebase!',
+  };
+  response.send(res);
+});
+
+export const helloWorld2 = functions.https.onRequest((_request, response) => {
+  functions.logger.info('Hello logs!', {structuredData: true});
+  const res: response = {
+    status: 200,
+    message: 'Hello from Firebase!',
+  };
+  response.send(res);
+});
