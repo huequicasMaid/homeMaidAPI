3b29c6288defc8402e8a0d3075159e9cce6f3a81
huequica <dev@huequica.xyz>
2021-11-23 19:34:44 +0900

    SwitchBotのリクエスト本体実装

---
+++ functions/src/service/devices.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import {response} from 'homeMaidApi';
+import {SwitchBotAPI} from 'config';
+
+const devices = async (): Promise<response | void> => {
+  const res = await axios.get<response>(`${SwitchBotAPI.BASE_URL}/devices`, {
+    headers: {
+      Authorization: SwitchBotAPI.API_TOKEN,
+    },
+  });
+  if (!res.data.body) return;
+  return res.data;
+};
+
+export default devices;
