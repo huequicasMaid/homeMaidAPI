5eb9f90b228a5a1ace12ccabf4c6ccb6c234f06f
huequica <dev@huequica.xyz>
2021-11-24 01:09:52 +0900

    config 情報の取得を実装

--- functions/src/config/switchBotApi.ts
+++ functions/src/config/switchBotApi.ts
@@ -1,2 +1,10 @@
+import {config} from 'firebase-functions';
+
 export const BASE_URL = 'https://api.switch-bot.com/v1.0';
-export const API_TOKEN = '';
+export const API_TOKEN = (): string | void => {
+  const configData = config();
+  if (!configData.switchbot_api_token || !configData.switchbot_api_token.key) {
+    return;
+  }
+  return configData.switchbot_api_token.key;
+};
