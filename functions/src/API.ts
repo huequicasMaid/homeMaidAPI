45f1571c3d92b3fa752d43a26b80a4db677c215a
huequica <dev@huequica.xyz>
2021-11-23 19:34:14 +0900

    rename

---
+++ functions/src/API.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import devices from 'service/devices';
+const app: express.Express = express();
+
+app.use(express.json);
+app.use(express.urlencoded({extended: true}));
+
+app.get('/devices', async (req: express.Request, res: express.Response) => {
+  const devicesResponse = await devices();
+
+  if (!devicesResponse) {
+    res.send({statusCode: 500, message: 'API REQUEST ERROR'});
+    return;
+  }
+
+  res.send({
+    statusCode: devicesResponse.statusCode,
+    body: devicesResponse.body,
+  });
+});
+
+export default app;
--- functions/src/express.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app: express.Express = express();
-
-app.use(express.json);
-app.use(express.urlencoded({extended: true}));
-
-app.get('/devices', (req, res) => {
-
-});
