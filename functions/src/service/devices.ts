e6597fc5ced0cd92186e8e8d038a84defbc7dca6
huequica <dev@huequica.xyz>
2021-11-24 01:11:31 +0900

    統合

--- functions/src/service/devices.ts
+++ functions/src/service/devices.ts
@@ -3,9 +3,12 @@ import {response} from 'homeMaidApi';
 import {SwitchBotAPI} from '@/config';
 
 const devices = async (): Promise<response | void> => {
+  const token = SwitchBotAPI.API_TOKEN();
+  if (!token) return;
+
   const res = await axios.get<response>(`${SwitchBotAPI.BASE_URL}/devices`, {
     headers: {
-      Authorization: SwitchBotAPI.API_TOKEN,
+      Authorization: token,
     },
   });
   if (!res.data.body) return;
