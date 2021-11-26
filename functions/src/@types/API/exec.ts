cda50faa4a98a379308f07e9a8ab59eab200f5a8
huequica <dev@huequica.xyz>
2021-11-27 06:23:43 +0900

    fix type of body interface

--- functions/src/@types/API/exec.ts
+++ functions/src/@types/API/exec.ts
@@ -1,12 +1,15 @@
+import express from 'express';
 declare module 'homeMaidApi' {
-  export interface execRequest {
-    // API TOKEN(WIP)
-    token?: string;
+  export interface execRequest extends express.Request {
+    body: {
+      // API TOKEN(WIP)
+      token?: string;
 
-    // Enable Light
-    isTurnOn?: 'true';
+      // Enable Light
+      isTurnOn?: true;
 
-    // Enable huequica's Room Air Conditioner, Light
-    withRoom?: 'true';
+      // Enable huequica's Room Air Conditioner, Light
+      withRoom?: true;
+    };
   }
 }
