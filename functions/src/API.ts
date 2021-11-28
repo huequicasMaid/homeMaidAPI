9934188a15311b26c5e1fb4f42158b0ae1ce173d
huequica <dev@huequica.xyz>
2021-11-28 18:58:35 +0900

    authentication implement

--- functions/src/API.ts
+++ functions/src/API.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { check, validationResult } from 'express-validator';
+import { body, check, validationResult } from 'express-validator';
 import { execRequest, helloRequest } from 'homeMaidApi';
 import { fetchUserFromToken } from '@/service/firestore/fetchUserFromToken';
 import devices from '@/service/devices';
@@ -47,15 +47,15 @@ app.get(
 
     if (!req.query.token) {
       return res
-        .status(500)
+        .status(401)
         .send({ statusCode: 500, message: 'missing token' });
     }
 
     const userResponse = await fetchUserFromToken(req.query.token);
     if (!userResponse) {
       return res
-        .status(500)
-        .send({ statusCode: 500, message: 'User not found.' });
+        .status(401)
+        .send({ statusCode: 401, message: 'User not found.' });
     }
 
     return res.send({
@@ -65,15 +65,28 @@ app.get(
   }
 );
 
-app.post('/exec', async (req: execRequest, res: express.Response) => {
-  // API TOKEN(WIP)
-  // if (!req.params.token) {
-  //   res.send({
-  //     statusCode: 401,
-  //     message: 'unAuthorized: Missing Token',
-  //   });
-  //   return;
-  // }
+app.post(
+  '/exec',
+  [body('token').notEmpty()],
+  async (req: execRequest, res: express.Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(401).send({ statusCode: errors.array() });
+    }
+
+    if (!req.body.token) {
+      return res
+        .status(401)
+        .send({ statusCode: 401, message: 'missing token' });
+    }
+
+    const userResponse = await fetchUserFromToken(req.body.token);
+    if (!userResponse) {
+      return res
+        .status(401)
+        .send({ statusCode: 401, message: 'User not found' });
+    }
+
   const sceneApiRequest = await exec(
     Boolean(req.body.isTurnOn),
     Boolean(req.body.withRoom)
