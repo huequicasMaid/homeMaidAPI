cf46be226150e8fb7cd2cac00f35da0a592e5edd
huequica <dev@huequica.xyz>
2021-11-28 21:41:43 +0900

    change region

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
 import app from './API';
 
-const api = functions.https.onRequest(app);
+const api = functions.region('asia-northeast1').https.onRequest(app);
 export { api };
