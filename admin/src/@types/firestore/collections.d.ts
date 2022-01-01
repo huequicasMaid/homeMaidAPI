ec74705c47ec3545b2b36c6291a4cbec876b9b52
huequica <dev@huequica.xyz>
2022-01-02 00:55:09 +0900

    fix naming and add timestamp property

--- admin/src/@types/firestore/collections.d.ts
+++ admin/src/@types/firestore/collections.d.ts
@@ -2,9 +2,11 @@ declare module 'homeMaidAdminFirestore' {
   export interface user {
     userName: string;
     tokenId: string;
+    createdAt: Date;
   }
 
   export interface token {
     token: string;
+    createdAt: Date;
   }
 }
