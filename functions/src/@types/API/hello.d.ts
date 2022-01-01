f8529bc7f352e96e7e05e227f139d2bab571f5b4
huequica <dev@huequica.xyz>
2022-01-01 23:59:38 +0900

    somefix

--- functions/src/@types/API/hello.d.ts
+++ functions/src/@types/API/hello.d.ts
@@ -1,8 +1,8 @@
 declare module 'homeMaidApi' {
   import express from 'express';
   export interface helloRequest extends express.Request {
-    query: {
-      token?: string;
+    headers: {
+      authorization?: string;
     };
   }
 }
