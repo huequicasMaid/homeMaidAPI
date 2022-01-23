98368e0b57bbe6fc0c524eb5900c1c8e8057b78e
huequica <dev@huequica.xyz>
2022-01-24 02:26:11 +0900

    remove `undefined` return and throw error exceptions

--- functions/src/service/exec.ts
+++ functions/src/service/exec.ts
@@ -1,17 +1,21 @@
 import axios from 'axios';
 import { execResponse } from 'homeMaidServices';
 import { SwitchBotAPI } from '@/config';
+import {
+  ExecutionFailedException,
+  SceneIdNotFoundException,
+} from '@/exception';
 
 const exec = async (
   isTurnOn: boolean,
   withRoom: boolean
-): Promise<{ data: execResponse; execPath: string } | void> => {
+): Promise<{ data: execResponse; execPath: string }> => {
   const sceneId = isTurnOn
     ? SwitchBotAPI.Scenes.comeBack(withRoom)
     : SwitchBotAPI.Scenes.goingOut(withRoom);
 
   const token = SwitchBotAPI.API_TOKEN();
-  if (!sceneId || !token) return;
+  if (!sceneId || !token) throw new SceneIdNotFoundException();
 
   const res = await axios.post<execResponse>(
     `${SwitchBotAPI.BASE_URL}/scenes/${sceneId}/execute`,
@@ -24,7 +28,8 @@ const exec = async (
   );
 
   // view StatusCode of HTTP Headers
-  if (res.status !== 200) return;
+  if (res.status !== 200) throw new ExecutionFailedException();
+
   return {
     data: res.data,
     execPath: `${SwitchBotAPI.BASE_URL}/scenes/${sceneId}/execute`,
