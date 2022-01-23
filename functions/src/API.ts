fcc3122b4947b28712ef6b9258b217af2f50dafd
huequica <dev@huequica.xyz>
2022-01-24 02:32:27 +0900

    use `tryCatch` and caught any error cases

--- functions/src/API.ts
+++ functions/src/API.ts
@@ -4,6 +4,11 @@ import { execRequest, helloRequest } from 'homeMaidApi';
 import { fetchUserFromToken } from '@/service/firestore/fetchUserFromToken';
 import exec from '@/service/exec';
 import { writeHistory } from '@/service/firestore/writeHistories';
+import {
+  ExecutionFailedException,
+  SceneIdNotFoundException,
+  UserNotFoundException,
+} from '@/exception';
 
 const app: express.Express = express();
 app.use(express.json({}));
@@ -52,39 +57,50 @@ app.post(
         .send({ statusCode: 401, message: 'missing token' });
     }
 
-    const userResponse = await fetchUserFromToken(req.headers.authorization);
-    if (!userResponse) {
-      return res
-        .status(401)
-        .send({ statusCode: 401, message: 'User not found' });
-    }
+    try {
+      const userResponse = await fetchUserFromToken(req.headers.authorization);
+      // request execute scene to switchBotAPI
+      const sceneApiRequest = await exec(
+        Boolean(req.body.isTurnOn),
+        Boolean(req.body.withRoom)
+      );
 
-    // request execute scene to switchBotAPI
-    const sceneApiRequest = await exec(
-      Boolean(req.body.isTurnOn),
-      Boolean(req.body.withRoom)
-    );
-
-    if (!sceneApiRequest) {
-      return res.send({
-        statusCode: 500,
-        message: 'API REQUEST ERROR',
+      // no wait to complete histories transaction
+      writeHistory({
+        category: 'apiExecute',
+        endpoint: sceneApiRequest.execPath,
+        user: userResponse,
+        result: sceneApiRequest.data,
       });
+
+      return res.send({
+        statusCode: 200,
+        message: sceneApiRequest.data.message,
+        body: sceneApiRequest.data.body,
+      });
+    } catch (error: unknown) {
+      if (error instanceof UserNotFoundException) {
+        return res
+          .status(401)
+          .send({ statusCode: 401, message: 'User not found' });
+      }
+
+      if (error instanceof SceneIdNotFoundException) {
+        return res
+          .status(500)
+          .send({ statusCode: 500, message: 'SceneId not found.' });
+      }
+
+      if (error instanceof ExecutionFailedException) {
+        return res
+          .status(500)
+          .send({ statusCode: 500, message: 'API REQUEST ERROR' });
+      }
+
+      return res
+        .status(500)
+        .send({ statusCode: 500, message: 'Caught Unhandled Exception.' });
     }
-
-    // no wait to complete histories transaction
-    writeHistory({
-      category: 'apiExecute',
-      endpoint: sceneApiRequest.execPath,
-      user: userResponse,
-      result: sceneApiRequest.data,
-    });
-
-    return res.send({
-      statusCode: 200,
-      message: sceneApiRequest.data.message,
-      body: sceneApiRequest.data.body,
-    });
   }
 );
 
